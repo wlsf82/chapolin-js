f9adfd644cf9c36c34f4964f99f3a111e5d58197
Walmyr <wlsf82@gmail.com>
2025-02-25 01:05:02 +0100

    Add new popular saying

--- dados/ditosPopulares.js
+++ dados/ditosPopulares.js
@@ -2,5 +2,6 @@ module.exports = [
   'Antes só do que dois voando',
   'Mais vale um pássaro na mão do que mal acompanhado',
   'Crie uma cobra e eu te direi quem és',
-  'Quem ri por último é quem demora mais pra entender a piada'
+  'Quem ri por último é quem demora mais pra entender a piada',
+  'Gato escaldado não se olha os dentes'
 ]
